75bb7ae81e945d69f9d9ca2b9289ee8585178039
Tim te Beek <tim@moderne.io>
2025-09-05 11:53:20 +0200

    Move additional test to migrate6 package

--- src/test/java/org/openrewrite/java/struts/migrate6/MigrateStruts6ConstantsTest.java
+++ src/test/java/org/openrewrite/java/struts/migrate6/MigrateStruts6ConstantsTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.openrewrite.java.struts;
+package org.openrewrite.java.struts.migrate6;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.InMemoryExecutionContext;
